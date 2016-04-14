2a69c39ef5d5e3dcffe5b0585fbea7c73c7ddb6c
lucabasilico.it@gmail.com <lucabasilico.it@gmail.com>
2016-04-14 19:21:10 +0200

    complete navbar

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+  def home
+
+  end
+
+  def abouts
+
+  end
+
+  def quality
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  root 'welcome#home'
+    root 'pages#home'
+    get 'about', to:"pages#about"
+    get 'quality', to:"pages#quality"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
